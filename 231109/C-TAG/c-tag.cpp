821024a876b8b8abe2e82a4fb37516154d1c5098
movingrock <75603892+movingrock@users.noreply.github.com>
2023-11-09 17:05:00 +0900

    Title: C-TAG, Time: 1162ms, Memory: 0MB, Status: Passed - CodeTree

--- 231109/C-TAG/c-tag.cpp
+++ 231109/C-TAG/c-tag.cpp
@@ -4,11 +4,25 @@
 
 using namespace std;
 
-int main(){
-    int n, m;
-    string groupA[500];
-    string groupB[500];
+int n, m;
+string groupA[501];
+string groupB[501];
+unordered_set<string> s1;
 
+bool Func(int i, int j, int k){
+    s1.clear();
+
+    for(int l = 0; l<n; l++){
+        s1.insert(groupA[l].substr(i, 1) + groupA[l].substr(j, 1) + groupA[l].substr(k, 1));
+    }
+    for(int l = 0; l<n; l++){
+        if(s1.find(groupB[l].substr(i, 1) + groupB[l].substr(j, 1) + groupB[l].substr(k, 1)) != s1.end())
+            return false;
+    }
+    return true;
+}
+
+int main(){
     cin >> n >> m;
     for(int i = 0; i<n; i++){
         cin >> groupA[i];
@@ -22,21 +36,12 @@ int main(){
     for(int i = 0; i<m; i++){
         for(int j = i+1; j<m; j++){
             for(int k = j+1; k<m; k++){
-                unordered_set<string> s1;
-                bool iscnt = true;
-                for(int l = 0; l<n; l++){
-                    s1.insert(groupA[l].substr(i, 1) + groupA[l].substr(j, 1) + groupA[l].substr(k, 1));
-                }
-                for(int l = 0; l<n; l++){
-                    if(s1.find(groupB[l].substr(i, 1) + groupB[l].substr(j, 1) + groupB[l].substr(k, 1)) != s1.end())
-                        iscnt = false;
-                }
-
-                if(iscnt) cnt++;
+                if(Func(i,j,k)) cnt++;
             }
             
         }
     }
+
     cout << cnt;
     return 0;
 }
